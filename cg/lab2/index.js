c75cea5c066007dc17afafce57fbd60c4be38ad8
beilanm <bielanm.box@gmail.com>
2017-11-02 02:31:03 +0200

    try tor

--- cg/lab2/index.js
+++ cg/lab2/index.js
@@ -1,5 +1,5 @@
 execute(() => {
-    const canvas = document.getElementById('canvas.lab#1'),
+    const canvas = document.getElementById('canvas.lab#2'),
         loader = new GLoader(canvas);
 
     loader.initVertexShader('vertex.lab#2');
@@ -14,11 +14,9 @@ execute(() => {
 
     const modelMatrix = mat4.create();
     const perspectiveMatrix = mat4.create();
-    const vertexContaineer = new VertexContainer(3, [
-        0.0,  1.0,  -3.0,
-        -1.0, -1.0,  -5.0,
-        1.0, -1.0,  -2.0
-    ]);
+    const vertexContaineer = new VertexContainer(3, getTorVertex(0.7, 0.2, 30));
+
+    mat4.identity(modelMatrix);
 
     function drawSceneCallback(ctx) {
         const { gl } = ctx;
@@ -27,8 +25,8 @@ execute(() => {
         gl.clearColor(0.0, 0.0, 0.0, 0.5);
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-        mat4.identity(modelMatrix);
-        mat4.perspective(perspectiveMatrix, 60*Math.PI/180, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0);
+        mat4.translate(modelMatrix, modelMatrix, [0, 0, -2.5]);
+        mat4.perspective(perspectiveMatrix, 45*Math.PI/180, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0);
         const buffer = gl.buffers['vertexPosition'];
         gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexContaineer.source), gl.STATIC_DRAW);
@@ -37,9 +35,28 @@ execute(() => {
         ctx.setUniform('modelUniform', modelMatrix);
         ctx.setUniform('perspectiveUniform', perspectiveMatrix);
 
-        gl.drawArrays(gl.TRIANGLES, 0, vertexContaineer.count);
+        gl.drawArrays(gl.TRIANGLE_STRIP, 0, vertexContaineer.count);
     }
     loader.drawScene(drawSceneCallback);
+
+
+    function getTorVertex(R, r, steps) {
+        const array = [];
+
+        for(let i = 0; i < steps; i++) {
+            const alpha = 2*Math.PI*i/steps - Math.PI;
+            for(let j = 0; j < steps; j++) {
+                const betta = (2*Math.PI)*j/steps,
+                    x = (R + r*Math.cos(alpha))*Math.cos(betta),
+                    y = (R + r*Math.cos(alpha))*Math.sin(betta),
+                    z = r*Math.cos(alpha);
+
+                array.push(x, y, z);
+            }
+        }
+
+        return array;
+    }
 
 });
 
